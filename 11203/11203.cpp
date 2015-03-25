e2426905467e9e586d1aec02ffe115542e673943
Pennliu <penn11.liu@gmail.com>
2015-03-25 23:07:44 +0800

    Update 11203.cpp

--- 11203/11203.cpp
+++ 11203/11203.cpp
@@ -25,9 +25,19 @@
 
 using namespace std;
 
-bool char_invalied(char c)
+bool check(string s)
 {
-    return ((c != 'M') && (c != 'E') && (c != '?'));
+    int a, b, c, i;
+    a = b = c = i = 0;
+    while (s[i] == '?') a++, i++;
+    if (s[i] != 'M') return false;
+    i++;
+    while (s[i] == '?') b++, i++;
+    if (s[i] != 'E') return false;
+    i++;
+    while (s[i] == '?') c++, i++;
+    if (s[i] != 0) return false;
+    return (a && b && c && (a == (c - (b - 1) - 1)));
 }
 int main()
 {
@@ -36,98 +46,14 @@ int main()
 #endif
     int n;
     cin >> n;
-    typedef enum {
-        idle = 0,
-        question_mark_1,
-        M_mark,
-        question_mark_2,
-        E_mark,
-        question_mark_3,
-        ok
-    } state;
+
     while (n--)
     {
-        state s = idle;
-        string str;
-        cin >> str;
-        bool flag = true;
-        int cnt_middle = 0;
-        int cnt_right= 0;
-        for (int i = 0; i < str.length(); i++)
-        {
-            if (char_invalied(str[i])) 
-            {
-                flag = false;
-                break;
-            }
-            switch(s)
-            {
-                case idle:
-                {
-                    if (str[i] == '?') s = question_mark_1;
-                    else {
-                        flag = false;
-                    }
-                    break;
-                }
-                case question_mark_1:
-                {
-                    if (str[i] == 'M') s = M_mark;
-                    else if (str[i] == 'E')
-                    {
-                        flag = false;
-                    }
-                    break;
-                }
-                case M_mark:
-                {
-                    if (str[i] == '?') {s= question_mark_2;cnt_middle++;}
-                    else
-                    {
-                        flag = false;
-                    }
-                    break;
-                }
-                case question_mark_2:
-                {
-                    if (str[i] == 'E') s = E_mark;
-                    else if (str[i] == 'M') 
-                    {
-                        flag = false;
-                    }
-                    else {cnt_middle++;}
-                    break;
-                }
-                case E_mark:
-                {
-                    if (str[i] == '?') {s = ok; cnt_right++;}
-                    else
-                    {
-                        flag = false;
-                    } 
-                    break;
-                }
-                case ok:
-                {
-                    if (str[i] != '?')
-                    {
-                        flag = false;
-                    }
-                    else
-                    {
-                        cnt_right++;
-                    }
-                    break;
-                }
-                default:
-                assert(0);
-            }
-            if (flag == false)
-            {
-                break;
-            }
-        }
-        if (flag && ((cnt_right - cnt_middle) >= 1)) cout << "theorem" << endl;
-        else cout << "no-theorem" << endl;
+        string s;
+        cin >> s;
+        if (check(s)) cout << "theorem";
+        else cout << "no-theorem";
+        cout << endl;
     }
+    return 0;
 }
