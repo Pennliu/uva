48c319310e8fdc5ee4df1affca5b6578f4125319
Pennliu <penn11.liu@gmail.com>
2015-04-01 21:43:59 +0800

    update

---
+++ 264/264.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("in", "r", stdin);
+#endif
+	int n;
+	while (cin >> n)
+	{
+		int sum = 0;
+		int cnt = 1;
+		while (sum + cnt < n)
+		{
+			sum += cnt;
+			cnt++;
+		}
+		int index = n - sum;
+		int l, r;
+		if (cnt % 2)
+		{
+			l = cnt + 1 - index;
+			r = cnt + 1 - l;
+		}
+		else
+		{
+			r = cnt + 1 - index;
+			l = cnt + 1 - r;
+		}
+		printf("TERM %d is %d/%d\n", n, l, r);
+	}
+	return 0;
+}
