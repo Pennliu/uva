3a18e19bd36dbefa2821354eebbc58593fa22dd3
Pennliu <penn11.liu@gmail.com>
2015-05-01 14:27:20 +0800

    updated

--- 127/127.cpp
+++ 127/127.cpp
@@ -1,86 +1,124 @@
+/* 
+ * pack :
+ * add a pile
+ * del a pile
+ * pile :
+ * add a card
+ * del a card 
+ */
 #include <iostream>
 #include <string>
 #include <vector>
 #include <cstdio>
 
-class poker
+using namespace std;
+class card
 {
-	public:
-		char ch[2];
+    public:
+        char m_face;
+        char m_suit;
+    public:
+        card(char face = 0, char suit = 0):m_face(face), m_suit(suit){};
+        bool operator==(const card& a);
 };
-
-bool match(const poker& a, const poker& b)
+bool card::operator==(const card& a)
 {
-	return ((a.ch[0] == b.ch[0]) || (a.ch[1] == b.ch[1]));
+    return ((m_face == a.m_face) || (m_suit == a.m_suit));
 }
-
-typedef std::vector<poker> Pile;
-typedef std::vector<Pile> Piles;
-Piles piles;
-
+class pack
+{
+    public:
+        card c[52];
+};
+typedef vector<card> pile;
+typedef vector<pile> piles;
+static pack s_pack;
+static piles s_piles;
+void init()
+{
+    s_piles.clear();
+}
+void solve()
+{
+    for (int i = 0; i < 52; i++)
+    {
+        pile p;
+        p.push_back(s_pack.c[i]);
+        s_piles.push_back(p);
+        int last_index = s_piles.size() - 1;
+        int index = last_index;
+        while (index <= last_index)
+        {
+            card c = s_piles[index].back();
+            int tmp_index = index;
+            while (true)
+            {
+                if (index >= 3 && (c == s_piles[index-3].back()))
+                {
+                    index = index - 3;
+                    continue;
+                }
+                else if (index >= 1 && (c == s_piles[index-1].back()))
+                {
+                    index = index - 1;
+                    continue;
+                }
+                else
+                {
+                    if (index != tmp_index)
+                    {
+                        s_piles[index].push_back(c);
+                        s_piles[tmp_index].pop_back();
+                        if (s_piles[tmp_index].empty())
+                        {
+                            piles::iterator it = s_piles.begin() + tmp_index;
+                            s_piles.erase(it);
+                            last_index--;
+                        }
+                    }
+                    index++;
+                    break;
+                }
+            }
+        }
+    }
+    int size = s_piles.size();
+    printf("%d pile", size);
+    if (size > 1)
+    {
+        printf("s");
+    }
+    printf(" remaining:");
+    for (int i = 0; i < size; i++)
+    {
+        printf(" %lu", s_piles[i].size());
+    }
+    printf("\n");
+}
 int main()
 {
-#ifndef ONLINE_JUDGE
-	freopen("in", "r", stdin);
+#ifndef _ONLINE_JUDGE_
+    freopen("in", "r", stdin);
 #endif
-	while (true)
-	{
-		std::string s1;
-		std::string s2;
-		std::getline(std::cin, s1);
-		if (s1[0] == '#') break;
-		std::getline(std::cin, s2);
-		std::string s = s1 + " " + s2;
-		int i = 0;
-		piles.clear();
-		while (s[i] != 0)
-		{
-			poker p;
-			p.ch[0] = s[i++];
-			p.ch[1] = s[i++];
-			i++;
-			Pile pile;
-			pile.push_back(p);
-			piles.push_back(pile);
-			for (int j = piles.size()-1; j < piles.size(); j++)
-			{
-				int k = j;
-				poker b = piles[j].back();
-				while (true)
-				{
-					if (k - 3 >= 0)
-					{
-						poker a = piles[k-3].back();
-						if (match(a, b)) {
-							k -= 3;
-							continue;
-						}
-					}
-					if (k - 1 >= 0){
-						poker a = piles[k-1].back();
-						if (match(a, b)){
-							k -= 1;
-							continue;
-						}
-					}
-					break;
-				}
-				if (k != j) {
-					piles[k].push_back(b);
-					piles[j].pop_back();
-					if (piles[j].empty()) piles.erase(piles.begin() + j);
-				}
-				j = k;
-			}
-		}
-		std::cout << piles.size() << " " << "pile";
-		if (piles.size() > 1) std::cout << "s";
-		std::cout << " remaining:";
-		for (int j = 0; j < piles.size(); j++)
-		{
-			std::cout << " " << piles[j].size();
-		}
-		std::cout << std::endl;
-	}
-		return 0;
+/* 1. input
+ * 2. solve
+ * 3. output*/
+    while (true)
+    {
+        init();
+        char a, b;
+        scanf("%c", &a); 
+        if (a == '#') break;
+        else scanf("%c", &b);
+        s_pack.c[0].m_face = a;
+        s_pack.c[0].m_suit = b;
+        getchar();
+        for (int i = 1; i < 52; i++)
+        {
+            scanf("%c%c", &s_pack.c[i].m_face, &s_pack.c[i].m_suit);
+            getchar();
+        }
+        solve();
+    }
+    return 0;
 }
