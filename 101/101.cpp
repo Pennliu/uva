49d3159c8588a737ce9e84576bc271cc07f602c6
Pennliu <penn11.liu@gmail.com>
2015-05-03 10:55:54 +0800

    ac

--- 101/101.cpp
+++ 101/101.cpp
@@ -25,60 +25,111 @@
 #include <cassert>
 
 using namespace std;
-
-typedef vector<int> blocks;
-typedef vector<blocks> blocks_world;
-typedef vector<int> blocks_tracer;
-
-blocks_tracer s_location;
-blocks_world s_blocks_world;
-//define blocks_elt and pile_stack
-void blocks_world_init(int n)
+const int maxn = 30;
+class blocks
 {
-    s_blocks_world.clear();
-    s_location.clear();
-    for (int i = 0; i < n; i++)
+public:
+    int m_s[maxn];
+    int m_top;
+public:
+    blocks(int top = -1):m_top(top)
     {
-        blocks b;
-        b.push_back(i);
-        s_blocks_world.push_back(b);
-        s_location.push_back(i);
+        memset(m_s, 0, sizeof(m_s));
     }
-}
-void blocks_elt_top_to_a(int a)
+    void push(int a)
+    {
+        assert(m_top < maxn);
+        m_s[++m_top] = a;
+    }
+    void pop(void)
+    {
+        assert(m_top != -1);
+        m_top--;
+        return;
+    }
+    int top()
+    {
+        assert(m_top != -1);
+        return m_s[m_top];
+    }
+    int size()
+    {
+        return m_top + 1;
+    }
+};
+
+class blocks_world
 {
-    int location = s_location[a];
-    blocks& b = s_blocks_world[location];
-    int t = b.back();
+private:
+    blocks bs[maxn];
+public:
+    blocks_world()
+    {
+        for (int i = 0; i < maxn; i++)
+        {
+            bs[i].push(i);
+        }
+    }
+    blocks& get_blocks(int location)
+    {
+        return bs[location];
+    }
+};
+class blocks_tracer
+{
+private:
+    int m_t[maxn];
+public:
+    blocks_tracer()
+    {
+        for (int i = 0; i < maxn; i++) m_t[i] = i;
+    }
+    int get_location(int a)
+    {
+        return m_t[a];
+    }
+    void set_location(int a, int n)
+    {
+        m_t[a] = n;
+    }
+};
+
+//define blocks_elt and pile_stack
+void blocks_elt_top_to_a(blocks_tracer& ta, blocks_world& w, int a)
+{
+    int location = ta.get_location(a);
+    blocks& b = w.get_blocks(location);
+    int t = b.top();
     while (t != a)
     {
-        b.pop_back();
-        s_blocks_world[t].push_back(t);
-        s_location[t] = t;
+        b.pop();
+        w.get_blocks(t).push(t);
+        ta.set_location(t, t);
+        t = b.top();
     }
 }
-void blocks_stack_a_to_b(int a, int b)
+void blocks_stack_a_to_b(blocks_tracer& ta, blocks_world& w, int a, int b)
 {
-    int location_a = s_location[a];
-    int location_b = s_location[b];
-    blocks& b_a = s_blocks_world[location_a];
-    blocks& b_b = s_blocks_world[location_b];
+    int location_a = ta.get_location(a);
+    int location_b = ta.get_location(b);
+    blocks& b_a = w.get_blocks(location_a);
+    blocks& b_b = w.get_blocks(location_b);
     stack<int> s;
-    int t = b_a.back(); 
+    int t = b_a.top(); 
     while (t != a)
     {
         s.push(t);
-        b_a.pop_back();
-        t = b_a.back();
+        b_a.pop();
+        t = b_a.top();
     }
     s.push(t);
-    b_a.pop_back();
+    b_a.pop();
     while (!s.empty())
     {
         t = s.top();
         s.pop();
-        b_b.push_back(t);
-        s_location[t] = b;
+        b_b.push(t);
+        ta.set_location(t, location_b);
     }
 }
 
@@ -90,7 +141,8 @@ int main()
     int n;
     while (cin >> n)
     {
-        blocks_world_init(n);
+        blocks_tracer bt;
+        blocks_world bw;
         while (true)
         {
             string cmd1, cmd2;
@@ -99,32 +151,32 @@ int main()
             if (cmd1 == "quit") break;
             cin >> a >> cmd2 >> b;
             if (a == b) continue;
-            if (s_location[a] == s_location[b]) continue;
+            if (bt.get_location(a) == bt.get_location(b)) continue;
             if (cmd1 == "move")
             {
-                blocks_elt_top_to_a(a);
+                blocks_elt_top_to_a(bt, bw, a);
                 if (cmd2 == "onto")
                 {
-                    blocks_elt_top_to_a(b);
+                    blocks_elt_top_to_a(bt, bw, b);
                 }
-                blocks_stack_a_to_b(a, b);
+                blocks_stack_a_to_b(bt, bw, a, b);
             }
             else
             {
                 if (cmd2 == "onto")
                 {
-                    blocks_elt_top_to_a(b);
+                    blocks_elt_top_to_a(bt, bw, b);
                 }
-                blocks_stack_a_to_b(a, b);
+                blocks_stack_a_to_b(bt, bw, a, b);
             }
         }
         for (int i = 0; i < n; i++)
         {
             cout << i << ":";
-            int elts = s_blocks_world[i].size();
+            int elts = bw.get_blocks(i).size();
             for (int j = 0; j < elts; j++)
             {
-                cout << " " << s_blocks_world[i][j];
+                cout << " " << bw.get_blocks(i).m_s[j];
             }
             cout << endl;
         }
